54d744ef6bc763f0f85a845f227df768b17f39a3
j4lib <julia.beck@lir.gu.se>
2026-04-24 14:08:38 +0200

    Correct static path

--- contact/contact/settings.py
+++ contact/contact/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/contact/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_build')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
